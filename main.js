e1809076cae5618c4bd6d5505395e406367e2c3c
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 19:54:37 +0500

    sixth  Assignment

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assignmemt 07
+let additionResult = 5 + 3;
+console.log("additionResult", additionResult);
+let subtractionResult = 10 - 2;
+console.log("subtractionResult", subtractionResult);
+let multiplicationResult = 2 * 4;
+console.log("multiplicationResult", multiplicationResult);
+let divisionResult = 16 / 2;
+console.log("divisionResult", divisionResult);
---
+++ main.ts
@@ -0,0 +1,30 @@
+//Assignmemt 07
+
+let additionResult :number = 5+3;
+
+console.log("additionResult", additionResult);
+
+let subtractionResult :number = 10-2;
+
+console.log("subtractionResult", subtractionResult);
+
+let multiplicationResult :number = 2*4;
+
+console.log("multiplicationResult", multiplicationResult);
+
+let divisionResult :number = 16/2;
+
+console.log("divisionResult", divisionResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
